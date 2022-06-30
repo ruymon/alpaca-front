b5740469e3ba682defc3f7a7b1e664a855f19d5e
ruymon <ruystfrancis@gmail.com>
2022-06-30 01:11:30 -0300

    :bug: Add safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: ['bg-[#0D2C99]', 'bg-gradient-to-b from-[#2483C5] to-[#29B473]', 'bg-[#221E3B]'],
   theme: {
     extend: {
       fontFamily: {
